12a10b61691d4c4c80595a14fe0a018ffebb8161
Eduardo Huacac <eduardoahr2324@gmail.com>
2025-08-25 18:58:45 -0500

    Actividad 2

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -12,6 +12,7 @@ class TabsScreen extends StatelessWidget {
           child: Column(
             children: const [
               TabBar(
+                isScrollable: true,
                 labelColor: Colors.black,
                 unselectedLabelColor: Colors.grey,
                 tabs: [
@@ -19,15 +20,33 @@ class TabsScreen extends StatelessWidget {
                   Tab(text: 'Tab 2'),
                   Tab(text: 'Tab 3'),
                   Tab(text: 'Tab 4'),
+                  Tab(text: 'Tab 5'),
+                  Tab(text: 'Tab 6'),
+                  Tab(text: 'Tab 7'),
+                  Tab(text: 'Tab 8'),
+                  Tab(text: 'Tab 9'),
+                  Tab(text: 'Tab 10'),
                 ],
               ),
               Expanded(
                 child: TabBarView(
                   children: [
-                    Icon(Icons.directions_car_outlined, size: 100),
-                    Icon(Icons.directions_transit_filled_outlined, size: 100),
-                    Icon(Icons.directions_bike_sharp, size: 100),
-                    Icon(Icons.directions_walk, size: 100),
+                    Icon(
+                      Icons.directions_car_outlined,
+                      size: 100,
+                      color: Colors.amber,
+                    ),
+                    Icon(
+                      Icons.directions_transit_filled_outlined,
+                      size: 100,
+                      color: Colors.blueAccent,
+                    ),
+                    Icon(
+                      Icons.directions_bike_sharp,
+                      size: 100,
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    Icon(Icons.directions_walk, size: 100, color: Colors.teal),
                   ],
                 ),
               ),
