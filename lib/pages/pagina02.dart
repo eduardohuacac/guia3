4034e5242fe0445cb5a1af9f9577c47ff0afc676
Eduardo Huacac <eduardoahr2324@gmail.com>
2025-08-25 19:35:14 -0500

    Actividad 4

--- lib/pages/pagina02.dart
+++ lib/pages/pagina02.dart
@@ -15,6 +15,24 @@ class Pagina02 extends StatelessWidget {
               "Hola !",
               style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
+            Text(
+              "Firebase es una plataforma de desarrollo de aplicaciones móviles y web de Google que ofrece un conjunto de herramientas y servicios para simplificar la creación, el crecimiento y la monetización de aplicaciones. Incluye funcionalidades como bases de datos en tiempo real, autenticación de usuarios, alojamiento web, almacenamiento de archivos y análisis, todo gestionado sobre la infraestructura de Google Cloud",
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Regresar a la página anterior",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+              onPressed: () => (Navigator.pop(context)),
+            ),
           ],
         ),
       ),
